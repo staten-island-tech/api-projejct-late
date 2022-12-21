8b50bbe202d522f5e8615acb2d96671304979b4c
Nathan Chen <nathaniel.chen.1990@gmail.com>
2022-12-21 10:54:12 -0500

    asd

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -25,8 +25,8 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    @app.route("/")
+    def home():
+        return render_template('index.html')
 
     return app
--- peen.py
+++ peen.py
@@ -1,5 +1,5 @@
 import requests
 import json
 
-r = requests.get("https://pokeapi.co/api/v2/pokemon/charmander").json()
+r = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m").json()
 print(r)
