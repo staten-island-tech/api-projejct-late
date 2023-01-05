aa22560ec852bb04e15b12fd52d57fcce3f3244b
WhyAmIHere <93929879+AnythingIsBetterThanNoNameIGuess@users.noreply.github.com>
2023-01-05 10:02:14 -0500

    I hope this dont mess everything up

--- app/Scripts/activate.bat
+++ app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\justi\OneDrive\Documents\GitHub\api-projejct-late\app
+set VIRTUAL_ENV=C:\Users\Zeya.li\Documents\GitHub\api-projejct-late\app
 
 if not defined PROMPT set PROMPT=$P$G
 
