49af322c11b703167073536b18d267a581e59921
Nathan Chen <nathaniel.chen.1990@gmail.com>
2023-01-18 20:05:05 -0500

    fin.

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -30,23 +30,24 @@ def create_app(test_config=None):
     def getPost():
         if request.method == 'POST':
             title = request.form['title']
-            data = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?query={title}&number=100&apiKey=47893e0d5835489185dfb01313459ff5").json()
+            data = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?query={title}&number=100&apiKey=1e4441f3c6fb4946a47314219d7aeecb").json()
+            random = requests.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=1e4441f3c6fb4946a47314219d7aeecb').json()
             print(request.headers)
             if len(data['results']) == 0:
-                return render_template('noresults.html')
+                return render_template('noresults.html', random=random)
             else:
                 return render_template('search.html',data=data, search=title)
         else:
-            random = requests.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=47893e0d5835489185dfb01313459ff5').json()
-            random2 = requests.get('https://api.spoonacular.com/recipes/random?number=30&apiKey=47893e0d5835489185dfb01313459ff5').json()
+            random = requests.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=1e4441f3c6fb4946a47314219d7aeecb').json()
+            random2 = requests.get('https://api.spoonacular.com/recipes/random?number=30&apiKey=1e4441f3c6fb4946a47314219d7aeecb').json()
             return render_template('index.html', random=random, random2=random2)
     
     @app.route('/recipeinfo/<id>')
     def getId(id):
-        data = requests.get(f'https://api.spoonacular.com/recipes/{id}/information?includeNutrition=false&apiKey=47893e0d5835489185dfb01313459ff5').json()
-        random = requests.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=47893e0d5835489185dfb01313459ff5').json()
+        data = requests.get(f'https://api.spoonacular.com/recipes/{id}/information?includeNutrition=false&apiKey=1e4441f3c6fb4946a47314219d7aeecb').json()
+        random = requests.get('https://api.spoonacular.com/recipes/random?number=1&apiKey=1e4441f3c6fb4946a47314219d7aeecb').json()
         return render_template('info.html', data=data, random=random)
     
     return app
-    
+    #47893e0d5835489185dfbff013134595
     #8f76aac47cef4f23874744431bd6424a
